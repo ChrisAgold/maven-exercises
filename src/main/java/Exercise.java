8e433ceb4b7ed9bb758bd848cfebd9940338451b
Chris Agold <christophersagold@gmail.com>
2021-02-13 15:57:06 -0600

    added .gitignore

---
+++ src/main/java/Exercise.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise {
+    public static void main(String[] args) {
+
+    }
+
+}
